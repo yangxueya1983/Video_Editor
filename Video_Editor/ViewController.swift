8e4a9745e41d07afe995b9d7e209ea83323adb21
yy <02dzxxyy@163.com>
2024-11-01 15:59:42 -0700

    Initial Commit

---
+++ Video_Editor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Video_Editor
+//
+//  Created by Yu Yang on 2024-11-01.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Video_EditorTests/Video_EditorTests.swift
@@ -0,0 +1,17 @@
+//
+//  Video_EditorTests.swift
+//  Video_EditorTests
+//
+//  Created by Yu Yang on 2024-11-01.
+//
+
+import Testing
+@testable import Video_Editor
+
+struct Video_EditorTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
