02475cb528dde43c90ba0221b6263dae8046b84c
yy <02dzxxyy@163.com>
2024-11-02 18:06:39 -0700

    add task group test

--- Video_EditorTests/Video_EditorTests.swift
+++ Video_EditorTests/Video_EditorTests.swift
@@ -35,8 +35,43 @@ struct Video_EditorTests {
         }
     }
     
-    @Test func testMultiplePhoto2Video() async throws {
+    func generateVideo(image: UIImage, size: CGSize, duration: CMTime, url: URL) async throws -> Error? {
+        try await PhotoMediaUtility.createVideoFromImage(image: image, videoSize: size, duration: duration, outputURL:url)
+    }
+    
+    @Test("parallel photos to videos")
+    func testMultiplePhoto2Video() async throws {
+        var outputUrls: [URL] = []
         
+        let tmpDirectory = NSTemporaryDirectory()
+        
+        for i in 0..<10 {
+            let url = URL(fileURLWithPath: tmpDirectory + "test_\(i).mp4")
+            outputUrls.append(url)
+            
+            if FileManager.default.fileExists(atPath: url.path) {
+                _ = try FileManager.default.removeItem(at: url)
+            }
+        }
+        
+        var results: [(any Error)?] = []
+        
+        try await withThrowingTaskGroup(of: Error?.self) { group in
+            let image1 = UIImage(named: "pic_1.jpg")!
+            let videoSize = CGSizeMake(1024, 768)
+            let duration = CMTimeMake(value: 5, timescale: 1)
+            for url in outputUrls {
+                group.addTask {
+                    return try await generateVideo(image: image1, size: videoSize, duration: duration, url: url)
+                }
+            }
+            
+            for try await result in group {
+                results.append(result)
+            }
+        }
+        
+        #expect(results.allSatisfy{$0 == nil})
     }
 
 }
